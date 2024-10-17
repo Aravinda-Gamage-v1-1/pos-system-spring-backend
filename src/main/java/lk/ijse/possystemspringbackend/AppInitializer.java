6f5b80c11fa6fdecb6a845f885ea84f34af84cab
Aravinda-Gamage-v1-1 <aravindagamage21998@gmail.com>
2024-10-17 15:55:24 +0530

    add configs

--- src/main/java/lk/ijse/possystemspringbackend/AppInitializer.java
+++ src/main/java/lk/ijse/possystemspringbackend/AppInitializer.java
@@ -1,20 +1,29 @@
 package lk.ijse.possystemspringbackend;
 
+import jakarta.servlet.MultipartConfigElement;
+import jakarta.servlet.ServletRegistration;
+import lk.ijse.possystemspringbackend.config.WebAppConfig;
+import lk.ijse.possystemspringbackend.config.WebAppRootConfig;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 public class AppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
     @Override
     protected Class<?>[] getRootConfigClasses() {
-
+        return new Class[]{WebAppRootConfig.class};
     }
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        return new Class[0];
+        return new Class[]{WebAppConfig.class};
     }
 
     @Override
     protected String[] getServletMappings() {
-        return new String[0];
+        return new String[]{"/"};
+    }
+    @Override
+    protected void customizeRegistration(ServletRegistration.Dynamic registration) {
+        String tempDir = System.getProperty("java.io.tmpdir");
+        registration.setMultipartConfig(new MultipartConfigElement(tempDir));
     }
 }
---
+++ src/main/java/lk/ijse/possystemspringbackend/config/WebAppConfig.java
@@ -0,0 +1,21 @@
+package lk.ijse.possystemspringbackend.config;
+
+import jakarta.servlet.annotation.MultipartConfig;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan(basePackages = "lk.ijse.possystemspringbackend")
+@EnableWebMvc
+@EnableJpaRepositories(basePackages = "lk.ijse.possystemspringbackend")
+@EnableTransactionManagement
+@MultipartConfig(
+        fileSizeThreshold = 102*1024*2,
+        maxFileSize = 1024*1024*10,//10MB
+        maxRequestSize = 1024*1024*50 //50MB
+)
+public class WebAppConfig {
+}
