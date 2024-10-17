d871add4b09dfac9ad68819a36cef2d160d914f1
Aravinda-Gamage-v1-1 <aravindagamage21998@gmail.com>
2024-10-17 16:21:46 +0530

    add exception

---
+++ src/main/java/lk/ijse/possystemspringbackend/exception/CustoemrNotFound.java
@@ -0,0 +1,13 @@
+package lk.ijse.possystemspringbackend.exception;
+
+public class CustoemrNotFound extends RuntimeException {
+
+    public CustoemrNotFound() {
+    }
+
+    public CustoemrNotFound(String message) {
+    }
+
+    public CustoemrNotFound(String message, Throwable cause) {
+    }
+}
---
+++ src/main/java/lk/ijse/possystemspringbackend/exception/ItemNotFoundException.java
@@ -0,0 +1,12 @@
+package lk.ijse.possystemspringbackend.exception;
+
+public class ItemNotFoundException extends RuntimeException {
+    public ItemNotFoundException() {
+    }
+
+    public ItemNotFoundException(String message) {
+    }
+
+    public ItemNotFoundException(String message, Throwable cause) {
+    }
+}
